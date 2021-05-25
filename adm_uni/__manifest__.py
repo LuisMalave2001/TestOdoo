73514d9488e381a60b1554f80c1bde8aec9b42b6
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2021-05-25 15:45:52 +0200

    Update __manifest__.py

--- adm_uni/__manifest__.py
+++ adm_uni/__manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Admission',
-    'version': '0.1',
+    'version': '32',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'mail', 'website'],
